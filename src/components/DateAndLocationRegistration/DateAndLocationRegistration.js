9ac1a8567b3f6742075c5dacb97105da19493e19
long ho <long_ho@datahouse.com>
2021-04-26 11:17:08 +0700

    Working on testing registration module - 26.04.2021

--- src/components/DateAndLocationRegistration/DateAndLocationRegistration.js
+++ src/components/DateAndLocationRegistration/DateAndLocationRegistration.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
-
-import classes from './DateAndLocationRegistration.module.css';
+import moment from 'moment';
 
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import Select from 'react-select';
 
+import classes from './DateAndLocationRegistration.module.css';
+
 class DateAndLocationRegistration extends Component {
+  state = {
+    date: new Date(),
+    location: null
+  };
+
+  dateChangeHandler = date => {
+    console.log(moment(date).format('YYYY-MM-DD'));
+    this.setState({ ...this.state, date: date });
+  }
+
   render() {
     return (
-      <div className={classes.RegistrationContainer}>
-        <div className={classes.RegistrationHeader}>
-          <h3 className={classes.RegistrationHeader_Title}>Đăng ký xét nghiệm</h3>
-          <div></div>
-        </div>
-        <div className={classes.RegistrationBody}>
-          <div className={classes.RegistrationBody_Header}>
-            <h4>Thời gian và địa điểm xét nghiệm</h4>
+      <div className={classes.Container}>
+        <div className={classes.RegistrationContainer}>
+          <div className={classes.RegistrationHeader}>
+            <h2 className={classes.RegistrationHeader_Title}>Đăng ký xét nghiệm</h2>
           </div>
-          <div className={classes.RegistrationBody_Date}>
-            <h5>Chọn ngày bạn muốn xét nghiệm</h5>
-            <DatePicker selected={new Date()} />
-          </div>
-          <div className={classes.RegistrationBody_Location}>
-            <h5>Chọn địa điểm bạn muốn xét nghiệm</h5>
-            <Select options={this.props.listLocation} />
+          <div className={classes.RegistrationBody}>
+            <div className={classes.RegistrationBody_Header}>
+              <h3>Thời gian và địa điểm xét nghiệm</h3>
+            </div>
+            <div className={classes.RegistrationBody_Content}>
+              <div className={classes.RegistrationBody_Date}>
+                <h4 style={{ color: "#a19f9f" }}>Chọn ngày bạn muốn xét nghiệm</h4>
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.dateChangeHandler}
+                />
+              </div>
+              <div className={classes.RegistrationBody_Location}>
+                <h4 style={{ color: "#a19f9f" }}>Chọn địa điểm bạn muốn xét nghiệm</h4>
+                <Select options={this.props.listLocation} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
