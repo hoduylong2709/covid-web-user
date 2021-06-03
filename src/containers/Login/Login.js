9c40ceda81f12c25f7fc042b47832d092a8d839d
long ho <long_ho@datahouse.com>
2021-06-03 15:29:30 +0700

    Change regex email validation - 03.06.2021

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -69,7 +69,8 @@ class Login extends Component {
     }
 
     if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      // const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
       isValid = pattern.test(value) && isValid;
     }
 
